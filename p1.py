fe9b4ffdafbd04e042611e7d544e2dc8d3ebc7df
Salar Satti <salarsattiss@gmail.com>
2018-04-27 16:53:04 -0400

    fully working, nice printing, added readme file

--- p1.py
+++ p1.py
@@ -38,40 +38,60 @@ def max_action_over_sum(index, all_actions, U):
         #element wise multiplication
         action_expectation = sum([P_s_prime_given_s_a[j] * U[j] for j in range(len(U))])
         all_action_expectation.append(action_expectation)
-    return max(all_action_expectation)
+    max_action_util = max(all_action_expectation)
+    return [max_action_util, all_action_expectation.index(max_action_util)]
 
-
-file_name = "GW2.txt"
-
-size, rewards, Left, Up, Right, Down = get_all_MDP_data (file_name)
-
-all_actions = [Left, Up, Right, Down]
-
-#initialize Utility array 
-U1 = [0] * size
-gamma = 1
-epsilon = 0.001
-
-
-k = 0
-
-while(True):
-    U = U1.copy()
-    delta = 0
+def run_value_iteration (file_name):
+    print("Results for " + file_name)
+    size, rewards, Left, Up, Right, Down = get_all_MDP_data (file_name)
     
-    for i in range(size):
-        U1[i] = rewards[i] + gamma * max_action_over_sum(i, all_actions, U)
-        delta = max(delta, abs(U1[i] - U[i]))
-
-    if (delta < epsilon):
-        break
+    all_actions = [Left, Up, Right, Down]
+    
+    #initialize Utility array 
+    U1 = [0] * size
+    s_action = [0] * size
+    gamma = 1
+    epsilon = 0.001
+    
+    #Value iteration algorithm
+    k = 0
+    while(True):
+        U = U1.copy()
+        delta = 0
+        
+        for i in range(size):
+            max_action_util, max_action = max_action_over_sum(i, all_actions, U)
+            U1[i] = rewards[i] + gamma * max_action_util
+            s_action[i] = max_action
+            delta = max(delta, abs(U1[i] - U[i]))
+    
+        if (delta < epsilon):
+            break
+        
+        k = k + 1
+        if (k > 10000):
+            print("Taking too long - broke early!")
+            break
+        
+    print("State utilities")
+    print(U1)
+    
+    direction_action = []
+    #last state is abosrbing state so we dont care about that
+    for i in s_action[0:-1]:
+        if (i==0):
+            direction_action.append("Left")
+        if (i==1):
+            direction_action.append("Up")
+        if (i==2):
+            direction_action.append("Right")
+        if (i==3):
+            direction_action.append("Down")
+    print("Optimal policies")        
+    print(direction_action)
     
     
-    
-    k = k + 1
-    if (k > 10000):
-        print("k BREAK!")
-        break
-    
-    
-print(U1)
+run_value_iteration("GW1.txt")
+print("\n")
+run_value_iteration("GW2.txt")
+        
